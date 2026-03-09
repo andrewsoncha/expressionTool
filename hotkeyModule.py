0eb2ef5bd1cd0106c03f5c92ceb02655bc61e709
andrewsoncha <65234283+andrewsoncha@users.noreply.github.com>
2026-03-09 01:23:22 -0400

    Added hotkey output module

--- hotkeyModule.py
+++ hotkeyModule.py
@@ -1,5 +1,20 @@
-import keyboard
+from pyautogui import keyDown, keyUp
 from outputModule import outputModule
 
 class hotkeyModule(outputModule):
+    def __init__(self, emotionHotkeys=None):
+        self.emotionHotkeys = {}
+        if emotionHotkeys is not None:
+            self.emotionHotkeys = emotionHotkeys
 
+    def updateHotkeys(self, newHotkeyDict):
+        self.emotionHotkeys = newHotkeyDict
+
+    def outputHotkey(self, emotion):
+        if emotion not in self.emotionHotkeys:
+            print('hotkeyModule.outputHotkey: no hotkey for emotion {}!'.format(emotion))
+        else:
+            hotkeyStr = self.emotionHotkeys[emotion]
+            print('pressing hotkeyStr:', hotkeyStr)
+            keyDown(hotkeyStr)
+            keyUp(hotkeyStr)
